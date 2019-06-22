41babd4d820e7cf9f895293e90e7370720f9b4e3
davidBorn <42901656+davidBorn@users.noreply.github.com>
2019-06-22 16:46:51 -0600

    Updates

---
+++ precache-manifest.84b93436844530f3f234535d9ac25476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8bfc8737dcd4aa93c45d437f9261c97",
+    "url": "/React-App-Practice--Ninjas-List-/static/media/co.a8bfc873.jpg"
+  },
+  {
+    "revision": "adab9aa5db502b939a64",
+    "url": "/React-App-Practice--Ninjas-List-/static/js/runtime~main.adab9aa5.js"
+  },
+  {
+    "revision": "d7e6bcca4a48f5ded324",
+    "url": "/React-App-Practice--Ninjas-List-/static/js/main.d7e6bcca.chunk.js"
+  },
+  {
+    "revision": "199cc5e9a443e5563655",
+    "url": "/React-App-Practice--Ninjas-List-/static/js/2.199cc5e9.chunk.js"
+  },
+  {
+    "revision": "d7e6bcca4a48f5ded324",
+    "url": "/React-App-Practice--Ninjas-List-/static/css/main.f97210ba.chunk.css"
+  },
+  {
+    "revision": "eb0ecf77fa8030c997ddbc0d91e16f8a",
+    "url": "/React-App-Practice--Ninjas-List-/index.html"
+  }
+];
